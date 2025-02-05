ef463f10040bd4910b3c403a68e5e7b6b5cc93ad
Septian Hadi Nugroho <septianhadinugroho4@gmail.com>
2025-02-05 16:41:09 +0700

    generate quantity number

--- src/App.jsx
+++ src/App.jsx
@@ -39,13 +39,7 @@ function Form() {
     <form className="add-form">
     <h3>Hari ini belanja apa kita?</h3>
     <div>
-      <select>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
+      <input type="number" min={1} defaultValue={1} placeholder="Jumlah" />
       <input type="text" placeholder="nama barang..." />
     </div>
     <button>Tambah</button>
